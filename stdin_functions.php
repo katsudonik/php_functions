b8e5050a82fb73a4cf961382c47feeb338340356
kaikeda <katsudonwork@gmail.com>
2017-07-22 12:54:55 +0900

    .

--- stdin_functions.php
+++ stdin_functions.php
@@ -19,44 +19,33 @@ function mapLine($line, $keyMap = array(), $delimiter = " "){
     return $_tmp;
 }
 
+function mapLines($_lines, $keyMap = array(), $delimiter = " "){
+    $records = array();
+    foreach($_lines as $_line){
+        $records[] = mapLine($_line, $keyMap, $delimiter);
+    }
+    return $records;
+}
+
+
 $linesAll = fetchAllStdin();
 $stock = mapLine($linesAll[0], array(500, 100, 50, 10));
 
-
-
-
-
 $_v = mapLine($linesAll[1], array('line_num'));
-
 $_lines = array_slice($linesAll, 2, $_v['line_num']);
-$records = array();
-foreach($_lines as $_line){
-    $records[] = mapLine($_line, array('price', 500, 100, 50, 10));
-}
 
-foreach($records as $record){
-
-}
+$records = mapLines($_lines, array('price', 500, 100, 50, 10));
 
 
-for($i = 1; $i <= $n; $i++){
+foreach ($records as $record){
     //お釣り金額を算出
 
-    $stock = mapLine($linesAll[0], array(500, 100, 50, 10));
-
-
-    $s = getVals();
-    $price = $s[0];
-    $payment[500] = $s[1];
-    $payment[100] = $s[2];
-    $payment[50] = $s[3];
-    $payment[10] = $s[4];
     $types = array(500, 100, 50, 10);
     $paymentSum = 0;
     foreach($types as $type){
-        $paymentSum += $type * $payment[$type];
+        $paymentSum += $type * $record[$type];
     }
-    $rest = $paymentSum - $price;//支払い不足は考慮しない
+    $rest = $paymentSum - $record['price'];//支払い不足は考慮しない
 
     //お釣りの支払い方を算出
     foreach($types as $type){
